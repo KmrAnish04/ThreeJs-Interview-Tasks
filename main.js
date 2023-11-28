0d5bab285ebb45b4e45062c57b6aaf2f30c99cd7
KmrAnish04 <anish.1903.kmr@gmail.com>
2023-11-28 21:40:46 +0530

    Fixed: Shadow pixalating

--- main.js
+++ main.js
@@ -22,17 +22,50 @@ scene.add(plane);
 const cubeGeometry = new THREE.BoxGeometry(2,2,2,20,20,20);
 const cubeMaterial = new THREE.MeshStandardMaterial({color: '#d61c3b'});
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube.position.set(0,0,2);
+cube.position.set(0,0,0);
 cube.castShadow = true;
 scene.add(cube);
 
 // Spot Light
-const spotLight = new THREE.SpotLight(undefined, 100, 30, 0.5,1,1);
-spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 1024; // Improves the shadow quality
-spotLight.shadow.mapSize.height = 1024;
-spotLight.position.set(-2, 10, 12);
-scene.add(spotLight);
+// const spotLight = new THREE.SpotLight(undefined, 100, 30, 0.4,1,1);
+// spotLight.castShadow = true;
+// spotLight.shadow.mapSize.width = 2048; // Improves the shadow quality
+// spotLight.shadow.mapSize.height = 2048;
+// spotLight.shadow.camera.left = -100
+// spotLight.shadow.camera.right = 0
+// spotLight.shadow.camera.top = 0
+// spotLight.shadow.camera.bottom = -0
+// // spotLight.shadow.bias = 0.001; // Adjust the shadow bias
+// // spotLight.shadow.radius = -0.001; // Adjust the shadow bias
+// spotLight.position.set(-2, 10, 12);
+// scene.add(spotLight);
+
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(spotLightHelper);
+// const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+// scene.add(spotLightCameraHelper);
+
+// Directional Light
+const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize.width = 2048; // Improves the shadow quality
+dirLight.shadow.mapSize.height = 2048;
+dirLight.shadow.bias = 0.0001;
+dirLight.shadow.radius = 0.0001;
+dirLight.shadow.camera.left = -2
+dirLight.shadow.camera.right = 2
+dirLight.shadow.camera.top = 2
+dirLight.shadow.camera.bottom = -2
+dirLight.shadow.camera.near = 1;
+dirLight.shadow.camera.far = 30;
+dirLight.position.set(-2, 7, -5);
+scene.add(dirLight);
+
+// Directional Light Helper
+// const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 1);
+// scene.add(dirLightHelper);
+// const dirLightShadowHelper = new THREE.CameraHelper(dirLight.shadow.camera);
+// scene.add(dirLightShadowHelper);
 
 
 // Camera
@@ -45,12 +78,23 @@ const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
 renderer.shadowMap.enabled = true;
 renderer.setPixelRatio(2);
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene,camera);
 
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
+
+// Window Resizing or Responsiveness
+window.addEventListener('resize', ()=>{
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width/sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera);
+})
 
 // To simulate the 3D Environment at every frame
 function animate(){
